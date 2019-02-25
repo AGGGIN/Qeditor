e4604d65a70454718d61fd00bb83c715af4557d6
AG <415204643@qq.com>
2019-02-25 19:51:39 +0800

    增加历史记录，当前操作的文档改成vuex

---
+++ src/renderer/views/config.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="p-index">
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        text: ''
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .p-doc{
+  }
+</style>
